f16f50324b2cb9647cc352cf7b6276cb101ec2bf
Ben Branta <benbranta@Hotmail.com>
2026-05-20 20:51:15 -0500

    Completed assignment 1. Verified all test cases passed. Completed reflection for assignment 1.

--- assignment1/assignment1.py
+++ assignment1/assignment1.py
@@ -13,3 +13,65 @@ print(type(name_pieces))
 
 assert name_pieces[0] == first_name_capitalized.split()[0]
 assert name_pieces[1] == last_name_capitalized.split()[0]
+
+
+# Task 2 (Computing)
+lime_density_grams_per_cup = 248
+grapefruit_density_grams_per_cup = 226.8
+grams_per_pound = 453.592
+oz_per_cup = 8
+ml_per_oz = 29.574
+
+ml_per_cup = oz_per_cup * ml_per_oz
+lime_density_grams_per_ml = lime_density_grams_per_cup / ml_per_cup
+lime_density_pounds_per_oz = lime_density_grams_per_cup / grams_per_pound / oz_per_cup
+mixed_density_grams_per_cup = (
+    lime_density_grams_per_cup + grapefruit_density_grams_per_cup
+) / 2
+mixed_mass_grams = 0.75 * mixed_density_grams_per_cup
+mixed_weight_pounds = mixed_mass_grams / grams_per_pound
+mixed_density_pounds_per_oz = mixed_density_grams_per_cup / oz_per_cup / grams_per_pound
+
+
+# Task 3 (Computing)
+# fmt: off
+# copy this verbatim into your .py file
+
+def generate_robot_message(first_name, last_name, possible_messages_init, possible_messages_term): #  <---- do not modify this line
+    import random  # gain access to the `random` library
+
+    # select a random integer
+    selection_init = random.randrange(len(possible_messages_init))
+    selection_term = random.randrange(len(possible_messages_term))
+
+    # extract the randomly selected init or term message part using []
+    message_init = possible_messages_init[selection_init]
+    message_term = possible_messages_term[selection_term]
+
+    # construct a message using f-string formatting
+    message = f"{message_init}, {first_name} {last_name}.  {message_term}" # 1️⃣ <---- Modify this line in Task 3 to also use `last_name`
+
+    return message  #to get data out of a function, we must use the `return` keyword
+
+# a hardcoded list of initial messages
+possible_messages_init = [
+    "greetings",
+    "cheerio",
+    "good day",
+    "uh oh",
+    "hello there",
+]  # 2️⃣ <---- add a thing to the end of this list
+
+# another hardcoded list of terminal messages
+possible_messages_term = [
+    "actually, i need to go over there now",
+    "wait, don't i know you",
+    "this conversation is so boring",
+    "i'm a robot, i don't have feelings",
+]  # 2️⃣ <---- add a thing to this list
+
+
+# 3️⃣ inputs are the lowercase names.
+message_result = generate_robot_message(first_name_capitalized, last_name_capitalized, possible_messages_init,
+    possible_messages_term,
+)
---
+++ assignment1/reflection1.md
@@ -0,0 +1,29 @@
+# Assignment 1 Reflection
+
+## 1. Which of the tools introduced so far have you had previous experience with? Which were new?
+
+**Answer:** I have had experience with Spyder and VS Code. I have used GitHub and pytest but not to the extent I will for this class. I like using pytest but I can see where my formatter/linter Ruff likes to break it.
+
+---
+
+## 2. What part of this assignment was the most difficult?
+
+**Answer:** I had to dive into the test script to better understand the exact reason my code was failing. I did like that so I learned more about pytest. VS Code has a lot of auto completion tools now and sometimes they were wrong. It's easy to auto complete without thinking.
+
+---
+
+## 3. To what extent did you find yourself looking things up online? Describe the tactics you used to find the information you needed -- and how you filtered out the content that wasn't relevant or helpful.
+
+**Answer:** I looked up the XOR operator using Google and it quickly told me what it was. I didn't have to filter much. I had to look up how to prevent my formatter from formatting task 3 lines which were too long. In that case I used ChatGPT and it gave a few examples of the comment I needed to add to turn the formatter off for a block of code. I knew what I was looking for and I thought ChatGPT would do a better job than a generic Google search for the formatter problem.
+
+---
+
+## 4. I hope you made allies and friends in doing this assignment. Did you work with others? What was that like?
+
+**Answer:** I did not reach out on Piazza yet. I'm slightly concerned with the pace and working with others is hard to coordinate with work. I think pytest is a frenemy. I do like getting experience with it though and I agree with test driven development.
+
+---
+
+## 5. Describe one thing that you learned while working on this lesson that stood out as useful or interesting.
+
+**Answer:** In DS701 I started using GitHub but for this class when I saw the repo online I forked the repo and found that extremely helpful. I want to use version control but this also organized all of the files for me. It was great. I never really used pytest and for this assignment I thought it was a good introduction to it. Easier code to understand what the test cases are looking for.
